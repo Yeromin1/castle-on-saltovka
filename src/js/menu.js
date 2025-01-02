f87611124f8f14fffe92a67d2cfa66e145ffedd3
Yeromin Dmytro <eremin.dmitriy@ukr.net>
2025-01-02 13:49:43 +0200

    new-starts

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,18 +1,20 @@
-(() => {
-    const refs = {
-      // Додати атрибут data-menu-open на кнопку відкриття
-    openModalBtn: document.querySelector("[data-menu-open]"),
-      // Додати атрибут data-menu-close на кнопку закриття
-    closeModalBtn: document.querySelector("[data-menu-close]"),
-      // Додати атрибут data-menu на бекдроп модалки
-    modal: document.querySelector("[data-menu]"),
-    };
+'use strict';
 
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
+// (() => {
+//     const refs = {
+//       // Додати атрибут data-menu-open на кнопку відкриття
+//     openModalBtn: document.querySelector("[data-menu-open]"),
+//       // Додати атрибут data-menu-close на кнопку закриття
+//     closeModalBtn: document.querySelector("[data-menu-close]"),
+//       // Додати атрибут data-menu на бекдроп модалки
+//     modal: document.querySelector("[data-menu]"),
+//     };
 
-    function toggleModal() {
-      // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-    refs.modal.classList.toggle("is-open");
-    }
-})();
+//     refs.openModalBtn.addEventListener("click", toggleModal);
+//     refs.closeModalBtn.addEventListener("click", toggleModal);
+
+//     function toggleModal() {
+//       // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+//     refs.modal.classList.toggle("is-open");
+//     }
+// })();

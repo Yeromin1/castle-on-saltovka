feb4b9095448239aeb0572ef441a75fe4d5eff67
Yeromin1 <eremin.dmitriy@ukr.net>
2025-06-10 20:54:36 +0000

    Deploying to gh-pages from @ Yeromin1/castle-on-saltovka@5eb6d9eba6aecced5ada7c4678c43a79032afdbc 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/menu-a71cff40.js";const r=document.querySelectorAll(".gallery");r.forEach(e=>{e.addEventListener("click",t=>{const c=t.target.getAttribute("srcset")||t.target.getAttribute("src");basicLightbox.create(`<img src="${c}" width="800" height="600">`).show()})});
+import"./assets/menu-d01e3e43.js";const r=document.querySelectorAll(".gallery");r.forEach(e=>{e.addEventListener("click",t=>{const c=t.target.getAttribute("srcset")||t.target.getAttribute("src");basicLightbox.create(`<img src="${c}" width="800" height="600">`).show()})});
 //# sourceMappingURL=commonHelpers.js.map

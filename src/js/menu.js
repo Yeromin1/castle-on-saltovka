9c564f2e4abec481444b9d2ea007aaad13a2518a
Yeromin Dmytro <eremin.dmitriy@ukr.net>
2025-06-11 19:44:12 +0300

    filter-menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,9 +1,11 @@
+// Додати атрибут data-menu-open на кнопку відкриття
 document
   .querySelectorAll('[filter-menu-open], [data-menu-open]')
   .forEach(button => {
     button.addEventListener('click', () => {
       const targetMenu = button.hasAttribute('filter-menu-open')
-        ? document.querySelector('[filter-menu]')
+        ? // Додати атрибут data-menu на бекдроп модалки
+          document.querySelector('[filter-menu]')
         : document.querySelector('[data-menu]');
 
       if (targetMenu) {
@@ -13,13 +15,14 @@ document
     });
   });
 
-// Закрытие меню
+// Додати атрибут data-menu-close на кнопку закриття
 document
   .querySelectorAll('[filter-menu-close], [data-menu-close]')
   .forEach(button => {
     button.addEventListener('click', () => {
       const targetMenu = button.hasAttribute('filter-menu-close')
-        ? document.querySelector('[filter-menu]')
+        ? // Додати атрибут data-menu на бекдроп модалки
+          document.querySelector('[filter-menu]')
         : document.querySelector('[data-menu]');
 
       if (targetMenu) {
@@ -28,48 +31,3 @@ document
       }
     });
   });
-
-// header.html
-// (() => {
-//   const refs = {
-//     // Додати атрибут data-menu-open на кнопку відкриття
-//     openModalBtn: document.querySelector('[data-menu-open]'),
-//     // Додати атрибут data-menu-close на кнопку закриття
-//     closeModalBtn: document.querySelector('[data-menu-close]'),
-//     // Додати атрибут data-menu на бекдроп модалки
-//     modal: document.querySelector('[data-menu]'),
-//   };
-
-//   if (!refs.openModalBtn || !refs.closeModalBtn || !refs.modal) {
-//     console.error('Один из элементов не найден');
-//     return;
-//   }
-
-//   refs.openModalBtn.addEventListener('click', toggleModal);
-//   refs.closeModalBtn.addEventListener('click', toggleModal);
-
-//   function toggleModal() {
-//     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-//     refs.modal.classList.toggle('is-open');
-//     document.body.classList.toggle('no-scroll');
-//   }
-// })();
-
-// filter.html
-// const filterToggleBtn = document.querySelector(
-//   '[filter-menu-open], [data-menu-open]'
-// );
-// const filterMenu = document.querySelector('[filter-menu], [data-menu]');
-// const filterCloseBtn = document.querySelector(
-//   '[filter-menu-close], [data-menu-close]'
-// );
-
-// filterToggleBtn.addEventListener('click', function () {
-//   filterMenu.classList.add('is-open');
-//   document.documentElement.classList.add('no-scroll');
-// });
-
-// filterCloseBtn.addEventListener('click', function () {
-//   filterMenu.classList.remove('is-open');
-//   document.documentElement.classList.remove('no-scroll');
-// });

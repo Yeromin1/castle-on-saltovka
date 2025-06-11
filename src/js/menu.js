90c6ec5899a3a8535e8458a6d903f3beaab827c1
Yeromin Dmytro <eremin.dmitriy@ukr.net>
2025-06-11 18:19:22 +0300

    menu-js

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,40 +1,75 @@
+document
+  .querySelectorAll('[filter-menu-open], [data-menu-open]')
+  .forEach(button => {
+    button.addEventListener('click', () => {
+      const targetMenu = button.hasAttribute('filter-menu-open')
+        ? document.querySelector('[filter-menu]')
+        : document.querySelector('[data-menu]');
+
+      if (targetMenu) {
+        targetMenu.classList.add('is-open');
+        document.documentElement.classList.add('no-scroll');
+      }
+    });
+  });
+
+// Закрытие меню
+document
+  .querySelectorAll('[filter-menu-close], [data-menu-close]')
+  .forEach(button => {
+    button.addEventListener('click', () => {
+      const targetMenu = button.hasAttribute('filter-menu-close')
+        ? document.querySelector('[filter-menu]')
+        : document.querySelector('[data-menu]');
+
+      if (targetMenu) {
+        targetMenu.classList.remove('is-open');
+        document.documentElement.classList.remove('no-scroll');
+      }
+    });
+  });
+
 // header.html
-(() => {
-  const refs = {
-    // Додати атрибут data-menu-open на кнопку відкриття
-    openModalBtn: document.querySelector('[data-menu-open]'),
-    // Додати атрибут data-menu-close на кнопку закриття
-    closeModalBtn: document.querySelector('[data-menu-close]'),
-    // Додати атрибут data-menu на бекдроп модалки
-    modal: document.querySelector('[data-menu]'),
-  };
+// (() => {
+//   const refs = {
+//     // Додати атрибут data-menu-open на кнопку відкриття
+//     openModalBtn: document.querySelector('[data-menu-open]'),
+//     // Додати атрибут data-menu-close на кнопку закриття
+//     closeModalBtn: document.querySelector('[data-menu-close]'),
+//     // Додати атрибут data-menu на бекдроп модалки
+//     modal: document.querySelector('[data-menu]'),
+//   };
 
-  if (!refs.openModalBtn || !refs.closeModalBtn || !refs.modal) {
-    console.error('Один из элементов не найден');
-    return;
-  }
+//   if (!refs.openModalBtn || !refs.closeModalBtn || !refs.modal) {
+//     console.error('Один из элементов не найден');
+//     return;
+//   }
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-    refs.modal.classList.toggle('is-open');
-    document.body.classList.toggle('no-scroll');
-  }
-})();
+//   function toggleModal() {
+//     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+//     refs.modal.classList.toggle('is-open');
+//     document.body.classList.toggle('no-scroll');
+//   }
+// })();
 
 // filter.html
-const filterToggleBtn = document.querySelector('[filter-menu-open]');
-const filterMenu = document.querySelector('[filter-menu]');
-const filterCloseBtn = document.querySelector('[filter-menu-close]');
+// const filterToggleBtn = document.querySelector(
+//   '[filter-menu-open], [data-menu-open]'
+// );
+// const filterMenu = document.querySelector('[filter-menu], [data-menu]');
+// const filterCloseBtn = document.querySelector(
+//   '[filter-menu-close], [data-menu-close]'
+// );
 
-filterToggleBtn.addEventListener('click', function () {
-  filterMenu.classList.add('is-open');
-  document.documentElement.classList.add('no-scroll');
-});
+// filterToggleBtn.addEventListener('click', function () {
+//   filterMenu.classList.add('is-open');
+//   document.documentElement.classList.add('no-scroll');
+// });
 
-filterCloseBtn.addEventListener('click', function () {
-  filterMenu.classList.remove('is-open');
-  document.documentElement.classList.remove('no-scroll');
-});
+// filterCloseBtn.addEventListener('click', function () {
+//   filterMenu.classList.remove('is-open');
+//   document.documentElement.classList.remove('no-scroll');
+// });

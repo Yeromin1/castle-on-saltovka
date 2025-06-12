a5f6ffb3afee1dc24c276dd9bf4180e628af1a0b
Yeromin Dmytro <eremin.dmitriy@ukr.net>
2025-06-12 23:21:10 +0300

    icon-filter-animation

--- src/js/filter.js
+++ src/js/filter.js
@@ -2,6 +2,22 @@ document.querySelectorAll('.filter-toggle').forEach(button => {
   button.addEventListener('click', () => {
     const targetId = button.dataset.target;
     const content = document.getElementById(targetId);
-    content.hidden = !content.hidden;
+    const icon = button.querySelector('.icon-filter');
+
+    if (content) {
+      content.hidden = !content.hidden;
+    }
+
+    if (icon) {
+      icon.classList.toggle('icon-filter-animation');
+    }
   });
 });
+
+// document.querySelectorAll('.filter-toggle').forEach(button => {
+//   button.addEventListener('click', () => {
+//     const targetId = button.dataset.target;
+//     const content = document.getElementById(targetId);
+//     content.hidden = !content.hidden;
+//   });
+// });

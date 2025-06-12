836a07cc1e4d109abd73a602ab39ad1ba157fb46
Yeromin1 <eremin.dmitriy@ukr.net>
2025-06-12 20:21:24 +0000

    Deploying to gh-pages from @ Yeromin1/castle-on-saltovka@a5f6ffb3afee1dc24c276dd9bf4180e628af1a0b 🚀

--- commonHelpers3.js
+++ commonHelpers3.js
@@ -1,2 +1,2 @@
-import"./assets/menu-f265ad14.js";document.querySelectorAll(".filter-toggle").forEach(e=>{e.addEventListener("click",()=>{const c=e.dataset.target,t=document.getElementById(c);t.hidden=!t.hidden})});
+import"./assets/menu-f265ad14.js";document.querySelectorAll(".filter-toggle").forEach(e=>{e.addEventListener("click",()=>{const i=e.dataset.target,t=document.getElementById(i),c=e.querySelector(".icon-filter");t&&(t.hidden=!t.hidden),c&&c.classList.toggle("icon-filter-animation")})});
 //# sourceMappingURL=commonHelpers3.js.map

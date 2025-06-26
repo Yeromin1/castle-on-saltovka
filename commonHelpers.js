628048a2947941149928f892aa6449148d5827de
Yeromin1 <eremin.dmitriy@ukr.net>
2025-06-26 07:25:53 +0000

    Deploying to gh-pages from @ Yeromin1/castle-on-saltovka@8c31bae7d05509825b17a2a85d4d9ca61eaa2cff 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import"./assets/menu-f265ad14.js";const r=document.querySelectorAll(".gallery");r.forEach(e=>{e.addEventListener("click",t=>{const c=t.target.getAttribute("srcset")||t.target.getAttribute("src");basicLightbox.create(`<img src="${c}" width="800" height="600">`).show()})});
+//# sourceMappingURL=commonHelpers.js.map

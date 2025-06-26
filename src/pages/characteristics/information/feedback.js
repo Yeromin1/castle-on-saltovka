a71095339dbab46f845e441ee331bce656bec68c
Yeromin Dmytro <eremin.dmitriy@ukr.net>
2025-06-26 23:37:51 +0300

    icon-frame

--- src/pages/characteristics/information/feedback.js
+++ src/pages/characteristics/information/feedback.js
@@ -94,7 +94,7 @@ function renderReviews() {
             <svg class="icon-star ${
               i < review.rating ? 'filled' : 'outlined'
             }" width="20" height="20">
-              <use href="../../../img/icons.svg#icon-frame"></use>
+              <use href="/img/icons.svg#icon-frame"></use>
             </svg>
           `
           ).join('')}

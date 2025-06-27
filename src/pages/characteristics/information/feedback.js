2568b4393fc49f8bf1f19b2ed285d75098de53ac
Yeromin Dmytro <eremin.dmitriy@ukr.net>
2025-06-27 09:23:22 +0300

    ad

--- src/pages/characteristics/information/feedback.js
+++ src/pages/characteristics/information/feedback.js
@@ -94,7 +94,7 @@ function renderReviews() {
             <svg class="icon-star ${
               i < review.rating ? 'filled' : 'outlined'
             }" width="20" height="20">
-              <use href="/svg/symbol-defs.svg#icon-star"></use>
+              <use href="/img/icons.svg#icon-star"></use>
             </svg>
           `
           ).join('')}

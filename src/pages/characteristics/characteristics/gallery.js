8494fad3df3f488eb59ee357dabc9bb1242e34ab
Yeromin Dmytro <eremin.dmitriy@ukr.net>
2025-07-16 18:39:18 +0300

    castle-on-saltovka

--- src/pages/characteristics/characteristics/gallery.js
+++ src/pages/characteristics/characteristics/gallery.js
@@ -11,34 +11,6 @@ window.addEventListener('load', () => {
 
   let lightbox;
 
-  // function rebuildLightboxGallery(clickedIndex) {
-  //   const items = Array.from(thumbnails);
-  //   const reordered = [
-  //     ...items.slice(clickedIndex),
-  //     ...items.slice(0, clickedIndex),
-  //   ];
-
-  //   lightboxContainer.innerHTML = '';
-
-  //   reordered.forEach(thumb => {
-  //     const fullUrl = thumb.dataset.full;
-  //     const alt = thumb.querySelector('img')?.alt || '';
-  //     const a = document.createElement('a');
-  //     a.href = fullUrl;
-  //     a.setAttribute('data-gallery', 'gallery');
-  //     a.setAttribute('data-title', alt);
-  //     lightboxContainer.appendChild(a);
-  //   });
-
-  //   if (lightbox) {
-  //     lightbox.destroy();
-  //   }
-  //   lightbox = new SimpleLightbox('[data-gallery="gallery"]', {
-  //     captionsData: 'title',
-  //     captionDelay: 250,
-  //   });
-  // }
-
   function rebuildLightboxGallery(clickedIndex) {
     const items = Array.from(thumbnails);
     const reordered = [

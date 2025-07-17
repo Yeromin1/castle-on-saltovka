af14e0f5bfc15efbd940e02478b76ed8af105344
Yeromin1 <eremin.dmitriy@ukr.net>
2025-07-17 18:18:43 +0000

    Deploying to gh-pages from @ Yeromin1/castle-on-saltovka@50931c85f605e02c82b8e361e5e0c7ff29506efc 🚀

--- commonHelpers7.js
+++ commonHelpers7.js
@@ -1,53 +1,53 @@
-import"./assets/menu-f265ad14.js";import{S as A,A as M}from"./assets/vendor-f7826fb8.js";window.addEventListener("load",()=>{const e=document.querySelector("a.main-lightbox"),n=document.getElementById("main-picture"),s=n.querySelector("img"),t=n.querySelector("source"),d=document.querySelectorAll("picture.thumbnail");document.querySelector(".lightbox-gallery");let l;function o(a){const c=Array.from(d),r=[...c.slice(a),...c.slice(0,a)];let i=document.querySelector(".lightbox-gallery");i?i.innerHTML="":(i=document.createElement("div"),i.classList.add("lightbox-gallery"),i.hidden=!0,document.body.appendChild(i)),r.forEach(v=>{var q;const k=v.dataset.full,$=((q=v.querySelector("img"))==null?void 0:q.alt)||"",p=document.createElement("a");p.href=k,p.setAttribute("data-gallery","gallery"),p.setAttribute("data-title",$),i.appendChild(p)}),l&&l.destroy(),l=new A('[data-gallery="gallery"]',{captionsData:"title",captionDelay:250})}function g(a){const c=d[a];if(!c)return;const r=c.querySelector("source"),i=c.querySelector("img");t&&r?(t.srcset=r.srcset,t.media=r.media):t&&(t.removeAttribute("srcset"),t.removeAttribute("media")),s.src=i.src,s.alt=i.alt,e.href=c.dataset.full,d.forEach(v=>v.classList.remove("active")),c.classList.add("active"),o(a)}d.forEach((a,c)=>{a.addEventListener("click",r=>{r.preventDefault(),g(c)})}),g(0)});const w=document.getElementById("list-equipment"),E=document.getElementById("selected-equipment"),D=E.querySelector(".selected-equipment "),S=E.querySelector(".icon-close"),C=document.querySelectorAll(".item-equipment");E.addEventListener("click",e=>{e.stopPropagation(),w.classList.toggle("active"),S.classList.toggle("rotated")});C.forEach(e=>{e.addEventListener("click",n=>{n.stopPropagation(),D.textContent=e.textContent,w.classList.remove("active"),S.classList.remove("rotated")})});document.addEventListener("click",()=>{w.classList.remove("active"),S.classList.remove("rotated")});new M(".acc-container",{duration:300});document.querySelectorAll(".tab-btn").forEach(e=>{e.addEventListener("click",()=>{document.querySelectorAll(".tab-btn").forEach(n=>n.classList.remove("active")),document.querySelectorAll(".list-tab").forEach(n=>n.classList.remove("active")),e.classList.add("active"),document.getElementById(e.dataset.tab).classList.add("active")})});const y=document.getElementById("reviewModal"),R=document.getElementById("openModalBtn"),T=document.getElementById("closeModalBtn"),x=document.getElementById("reviewForm"),I=document.getElementById("reviewsContainer"),H=document.getElementById("showMoreBtn"),L=document.getElementById("starRating"),h=new URL("/castle-on-saltovka/assets/icons-e70d3d57.svg",self.location);let m=JSON.parse(localStorage.getItem("reviews"))||[],b=3,u=0;R.onclick=()=>{y.style.display="flex",document.body.style.overflow="hidden"};T.onclick=()=>{y.style.display="none",document.body.style.overflow=""};window.onclick=e=>{e.target==y&&(y.style.display="none",document.body.style.overflow="")};L.innerHTML=Array.from({length:5},(e,n)=>`
+import"./assets/menu-f265ad14.js";import{S as M,A as D}from"./assets/vendor-f7826fb8.js";window.addEventListener("load",()=>{const e=document.querySelector("a.main-lightbox"),n=document.getElementById("main-picture"),o=n.querySelector("img"),t=n.querySelector("source"),r=document.querySelectorAll("picture.thumbnail");let l;const s=["/img/pages/home/popular/mobile/GoldenSoftOffice@2x.jpg","/img/pages/home/popular/mobile/GoldenSoft2Hotel@2x.jpg","/img/pages/home/popular/mobile/GoldenSoft2Office@2x.jpg","/img/pages/home/popular/mobile/GoldenSoftHotel@2x.jpg"];r.forEach((a,i)=>{a.dataset.full=s[i]});function h(a){const i=Array.from(r),m=[...i.slice(a),...i.slice(0,a)];let c=document.querySelector(".lightbox-gallery");c?c.innerHTML="":(c=document.createElement("div"),c.classList.add("lightbox-gallery"),c.hidden=!0,document.body.appendChild(c)),m.forEach(g=>{var x;const $=g.dataset.full,A=((x=g.querySelector("img"))==null?void 0:x.alt)||"",p=document.createElement("a");p.href=$,p.setAttribute("data-gallery","gallery"),p.setAttribute("data-title",A),c.appendChild(p)}),l&&l.destroy(),l=new M('[data-gallery="gallery"]',{captionsData:"title",captionDelay:250})}function b(a){const i=r[a];if(!i)return;const m=i.querySelector("source"),c=i.querySelector("img");t&&m?(t.srcset=m.srcset,t.media=m.media):t&&(t.removeAttribute("srcset"),t.removeAttribute("media")),o.src=c.src,o.alt=c.alt,e.href=i.dataset.full,r.forEach(g=>g.classList.remove("active")),i.classList.add("active"),h(a)}r.forEach((a,i)=>{a.addEventListener("click",m=>{m.preventDefault(),b(i)})}),b(0)});const w=document.getElementById("list-equipment"),S=document.getElementById("selected-equipment"),C=S.querySelector(".selected-equipment "),L=S.querySelector(".icon-close"),j=document.querySelectorAll(".item-equipment");S.addEventListener("click",e=>{e.stopPropagation(),w.classList.toggle("active"),L.classList.toggle("rotated")});j.forEach(e=>{e.addEventListener("click",n=>{n.stopPropagation(),C.textContent=e.textContent,w.classList.remove("active"),L.classList.remove("rotated")})});document.addEventListener("click",()=>{w.classList.remove("active"),L.classList.remove("rotated")});new D(".acc-container",{duration:300});document.querySelectorAll(".tab-btn").forEach(e=>{e.addEventListener("click",()=>{document.querySelectorAll(".tab-btn").forEach(n=>n.classList.remove("active")),document.querySelectorAll(".list-tab").forEach(n=>n.classList.remove("active")),e.classList.add("active"),document.getElementById(e.dataset.tab).classList.add("active")})});const y=document.getElementById("reviewModal"),R=document.getElementById("openModalBtn"),G=document.getElementById("closeModalBtn"),I=document.getElementById("reviewForm"),B=document.getElementById("reviewsContainer"),H=document.getElementById("showMoreBtn"),q=document.getElementById("starRating"),v=new URL("/castle-on-saltovka/assets/icons-e70d3d57.svg",self.location);let d=JSON.parse(localStorage.getItem("reviews"))||[],E=3,u=0;R.onclick=()=>{y.style.display="flex",document.body.style.overflow="hidden"};G.onclick=()=>{y.style.display="none",document.body.style.overflow=""};window.onclick=e=>{e.target==y&&(y.style.display="none",document.body.style.overflow="")};q.innerHTML=Array.from({length:5},(e,n)=>`
   <span data-index="${n}">
     <svg class="icon-star" width="20" height="20">
-      <use href="${h}#icon-star"></use>
+      <use href="${v}#icon-star"></use>
     </svg>
   </span>
-`).join("");L.addEventListener("click",e=>{const n=e.target.closest("span");n&&(u=parseInt(n.dataset.index)+1,B())});function B(){[...L.children].forEach((e,n)=>{e.classList.toggle("selected",n<u)})}x.onsubmit=function(e){e.preventDefault();const n=document.getElementById("nameInput").value,s=document.getElementById("commentInput").value;if(u===0){alert("Поставте оцінку");return}const t={id:Date.now(),name:n,comment:s,rating:u,date:new Date().toLocaleDateString("uk-UA",{day:"numeric",month:"long",year:"numeric"}),comments:[]};m.unshift(t),localStorage.setItem("reviews",JSON.stringify(m)),y.style.display="none",document.body.style.overflow="",x.reset(),u=0,B(),f()};function f(){I.innerHTML="",m.slice(0,b).forEach(s=>{const t=document.createElement("div");t.className="review";const d=s.comments.length;t.innerHTML=`
+`).join("");q.addEventListener("click",e=>{const n=e.target.closest("span");n&&(u=parseInt(n.dataset.index)+1,k())});function k(){[...q.children].forEach((e,n)=>{e.classList.toggle("selected",n<u)})}I.onsubmit=function(e){e.preventDefault();const n=document.getElementById("nameInput").value,o=document.getElementById("commentInput").value;if(u===0){alert("Поставте оцінку");return}const t={id:Date.now(),name:n,comment:o,rating:u,date:new Date().toLocaleDateString("uk-UA",{day:"numeric",month:"long",year:"numeric"}),comments:[]};d.unshift(t),localStorage.setItem("reviews",JSON.stringify(d)),y.style.display="none",document.body.style.overflow="",I.reset(),u=0,k(),f()};function f(){B.innerHTML="",d.slice(0,E).forEach(o=>{const t=document.createElement("div");t.className="review";const r=o.comments.length;t.innerHTML=`
       <ul class="list-review">
         <div class="container-item-name-date">
-          <li class="item-review-name">${s.name}</li>
-          <li class="item-review-date">${s.date}</li>
+          <li class="item-review-name">${o.name}</li>
+          <li class="item-review-date">${o.date}</li>
         </div>
         <li class="item-stars-view">
-          ${Array.from({length:5},(l,o)=>`
-            <svg class="icon-star ${o<s.rating?"filled":"outlined"}" width="20" height="20">
-              <use href="${h}#icon-star"></use>
+          ${Array.from({length:5},(l,s)=>`
+            <svg class="icon-star ${s<o.rating?"filled":"outlined"}" width="20" height="20">
+              <use href="${v}#icon-star"></use>
             </svg>
           `).join("")}
         </li>
       </ul>
 
       <div class="container-text-review-reply">
-        <p class="text-review-comment">${s.comment}</p>
+        <p class="text-review-comment">${o.comment}</p>
         <button class="">...more</button>
 
         <ul class="list-reply">
           <li class="item-reply">
             <svg class="icon-reply-btn" width="24" height="24">
-              <use href="${h}#icon-subdirectory"></use>
+              <use href="${v}#icon-subdirectory"></use>
             </svg>
           </li>
           <li class="item-reply">
-            <button class="reply-btn" data-id="${s.id}">
+            <button class="reply-btn" data-id="${o.id}">
               Відповісти
             </button></li>
           <li class="item-reply">
             <svg class="icon-toggle-comments" width="24" height="24">
-              <use href="${h}#icon-message"></use>
+              <use href="${v}#icon-message"></use>
             </svg>
           </li>
           <li class="item-comments">
-            <button class="toggle-comments" data-id="${s.id}">
-              ${d} Коментарі
+            <button class="toggle-comments" data-id="${o.id}">
+              ${r} Коментарі
             </button>
           </li>
         </ul>
       </div>
 
       <div class="comments" style="display: none;">
-        ${s.comments.map(l=>`<ul class="comment">
+        ${o.comments.map(l=>`<ul class="comment">
                 <li class="comment-name">${l.name}</li>
                 <li class="item-review-date">${l.date}</li>
                 <li class="comment-text">${l.text}</li>
@@ -59,5 +59,5 @@ import"./assets/menu-f265ad14.js";import{S as A,A as M}from"./assets/vendor-f782
         <textarea type="text" class="reply-text" placeholder="Ваш коментар" ></textarea>
         <button class="submit-reply btn-categories">Надіслати</button>
       </div>
-    `,t.querySelector(".reply-btn").addEventListener("click",l=>{l.preventDefault();const o=t.querySelector(".reply-form");o.style.display=o.style.display==="none"?"block":"none"}),t.querySelector(".toggle-comments").addEventListener("click",l=>{l.preventDefault();const o=t.querySelector(".comments");o.style.display=o.style.display==="none"?"block":"none"}),t.querySelector(".submit-reply").addEventListener("click",()=>{const l=t.querySelector(".reply-name").value.trim(),o=t.querySelector(".reply-text").value.trim();if(!l||!o)return alert("Введите имя и комментарий");const g=parseInt(t.querySelector(".reply-btn").dataset.id);m.find(c=>c.id===g).comments.push({name:l,text:o,date:new Date().toLocaleDateString("uk-UA",{day:"numeric",month:"long",year:"numeric"})}),localStorage.setItem("reviews",JSON.stringify(m)),f()}),I.appendChild(t)});const n=document.getElementById("showMoreContainer");n.style.display=m.length>b?"flex":"none"}H.onclick=()=>{b+=3,f()};f();
+    `,t.querySelector(".reply-btn").addEventListener("click",l=>{l.preventDefault();const s=t.querySelector(".reply-form");s.style.display=s.style.display==="none"?"block":"none"}),t.querySelector(".toggle-comments").addEventListener("click",l=>{l.preventDefault();const s=t.querySelector(".comments");s.style.display=s.style.display==="none"?"block":"none"}),t.querySelector(".submit-reply").addEventListener("click",()=>{const l=t.querySelector(".reply-name").value.trim(),s=t.querySelector(".reply-text").value.trim();if(!l||!s)return alert("Введите имя и комментарий");const h=parseInt(t.querySelector(".reply-btn").dataset.id);d.find(a=>a.id===h).comments.push({name:l,text:s,date:new Date().toLocaleDateString("uk-UA",{day:"numeric",month:"long",year:"numeric"})}),localStorage.setItem("reviews",JSON.stringify(d)),f()}),B.appendChild(t)});const n=document.getElementById("showMoreContainer");n.style.display=d.length>E?"flex":"none"}H.onclick=()=>{E+=3,f()};f();
 //# sourceMappingURL=commonHelpers7.js.map

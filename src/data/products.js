ebe1618f41f97cc96800d7a8de2cf3be4748c633
Yeromin Dmytro <eremin.dmitriy@ukr.net>
2025-07-18 19:09:22 +0300

    import-img-1

--- src/data/products.js
+++ src/data/products.js
@@ -2,6 +2,25 @@ const products = [
   {
     id: 1,
     category: 'Накладні електронні замки',
+    name: 'Дверний Замок Golden Soft для офісу',
+    description: 'Golden Soft для офісу',
+    price: 1000,
+    desktop: {
+      '1x': '/img/pages/home/popular/mobile/GoldenSoftOffice@1x.jpg',
+      '2x': '/img/pages/home/popular/mobile/GoldenSoftOffice@2x.jpg',
+    },
+    tablet: {
+      '1x': '/img/pages/home/popular/mobile/GoldenSoftOffice@1x.jpg',
+      '2x': '/img/pages/home/popular/mobile/GoldenSoftOffice@2x.jpg',
+    },
+    mobile: {
+      '1x': '/img/pages/home/popular/mobile/GoldenSoftOffice@1x.jpg',
+      '2x': '/img/pages/home/popular/mobile/GoldenSoftOffice@2x.jpg',
+    },
+  },
+  {
+    id: 2,
+    category: 'Накладні електронні замки',
     name: 'Варіативний замок Golden Soft для готелю',
     description: 'Golden Soft для готелю',
     price: 1000,
@@ -19,41 +38,22 @@ const products = [
     },
   },
   {
-    id: 2,
-    category: 'Накладні електронні замки',
-    name: 'Дверний Замок Golden Soft для офісу',
-    description: 'Golden Soft для офісу',
-    price: 1000,
-    desktop: {
-      '1x': '/img/pages/home/popular/mobile/GoldenSoft2Office@1x.jpg',
-      '2x': '/img/pages/home/popular/mobile/GoldenSoft2Office@2x.jpg',
-    },
-    tablet: {
-      '1x': '/img/pages/home/popular/mobile/GoldenSoft2Office@1x.jpg',
-      '2x': '/img/pages/home/popular/mobile/GoldenSoft2Office@2x.jpg',
-    },
-    mobile: {
-      '1x': '/img/pages/home/popular/mobile/GoldenSoft2Office@1x.jpg',
-      '2x': '/img/pages/home/popular/mobile/GoldenSoft2Office@2x.jpg',
-    },
-  },
-  {
     id: 3,
     category: 'Накладні електронні замки',
     name: 'Дверний Замок Golden Soft для офісу',
     description: 'Golden Soft для офісу',
     price: 1000,
     desktop: {
-      '1x': '/img/pages/home/popular/mobile/GoldenSoftHotel@1x.jpg',
-      '2x': '/img/pages/home/popular/mobile/GoldenSoftHotel@2x.jpg',
+      '1x': '/img/pages/home/popular/mobile/GoldenSoft2Office@1x.jpg',
+      '2x': '/img/pages/home/popular/mobile/GoldenSoft2Office@2x.jpg',
     },
     tablet: {
-      '1x': '/img/pages/home/popular/mobile/GoldenSoftHotel@1x.jpg',
-      '2x': '/img/pages/home/popular/mobile/GoldenSoftHotel@2x.jpg',
+      '1x': '/img/pages/home/popular/mobile/GoldenSoft2Office@1x.jpg',
+      '2x': '/img/pages/home/popular/mobile/GoldenSoft2Office@2x.jpg',
     },
     mobile: {
-      '1x': '/img/pages/home/popular/mobile/GoldenSoftHotel@1x.jpg',
-      '2x': '/img/pages/home/popular/mobile/GoldenSoftHotel@2x.jpg',
+      '1x': '/img/pages/home/popular/mobile/GoldenSoft2Office@1x.jpg',
+      '2x': '/img/pages/home/popular/mobile/GoldenSoft2Office@2x.jpg',
     },
   },
   {
@@ -63,16 +63,16 @@ const products = [
     description: 'Golden Soft для офісу',
     price: 1000,
     desktop: {
-      '1x': '/img/pages/home/popular/mobile/GoldenSoftOffice@1x.jpg',
-      '2x': '/img/pages/home/popular/mobile/GoldenSoftOffice@2x.jpg',
+      '1x': '/img/pages/home/popular/mobile/GoldenSoftHotel@1x.jpg',
+      '2x': '/img/pages/home/popular/mobile/GoldenSoftHotel@2x.jpg',
     },
     tablet: {
-      '1x': '/img/pages/home/popular/mobile/GoldenSoftOffice@1x.jpg',
-      '2x': '/img/pages/home/popular/mobile/GoldenSoftOffice@2x.jpg',
+      '1x': '/img/pages/home/popular/mobile/GoldenSoftHotel@1x.jpg',
+      '2x': '/img/pages/home/popular/mobile/GoldenSoftHotel@2x.jpg',
     },
     mobile: {
-      '1x': '/img/pages/home/popular/mobile/GoldenSoftOffice@1x.jpg',
-      '2x': '/img/pages/home/popular/mobile/GoldenSoftOffice@2x.jpg',
+      '1x': '/img/pages/home/popular/mobile/GoldenSoftHotel@1x.jpg',
+      '2x': '/img/pages/home/popular/mobile/GoldenSoftHotel@2x.jpg',
     },
   },
 ];
--- src/pages/characteristics/characteristics/gallery.js
+++ src/pages/characteristics/characteristics/gallery.js
@@ -1,11 +1,8 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-// Импорт изображений
-import img1 from '/img/pages/characteristics/overhead-electronic/mobile/1ectangle2x.png';
-import img2 from '/img/pages/home/popular/mobile/GoldenSoft2Hotel@2x.jpg';
-import img3 from '/img/pages/catalog/overhead-electronic/Desktop/3Rectangle2x.png';
-import img4 from '/img/pages/home/popular/mobile/GoldenSoftHotel@2x.jpg';
+import products from '../../../data/products.js';
+
 window.addEventListener('load', () => {
   const mainLink = document.querySelector('a.main-lightbox');
   const mainPicture = document.getElementById('main-picture');
@@ -15,7 +12,7 @@ window.addEventListener('load', () => {
 
   let lightbox;
 
-  const fullImages = [img1, img2, img3, img4];
+  const fullImages = products.map(product => product.desktop['2x']);
 
   // Присваиваем каждому thumbnail соответствующий full image
   thumbnails.forEach((thumb, index) => {

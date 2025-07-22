be9e284eed62306ee1bc8f273e4a7621a6b0b66d
Yeromin1 <eremin.dmitriy@ukr.net>
2025-07-22 16:15:03 +0000

    Deploying to gh-pages from @ Yeromin1/castle-on-saltovka@8c31fb68f51f3e8c395cd0712d9e52d6dba108c7 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,7 +1,7 @@
-import"./assets/menu-f265ad14.js";import r from"https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";document.addEventListener("DOMContentLoaded",()=>{new r(".premium-swiper",{loop:!0,slidesPerView:1,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",clickable:!0,renderBullet:function(t,e){return`
+import"./assets/menu-f265ad14.js";import s from"https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";const n=new URL("/castle-on-saltovka/assets/icons-e70d3d57.svg",self.location);document.addEventListener("DOMContentLoaded",()=>{new s(".premium-swiper",{loop:!0,slidesPerView:1,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",clickable:!0,renderBullet:function(t,e){return`
           <li class="${e}">
             <svg class="ball-icon" width="4" height="4">
-              <use href="./img/icons.svg#icon-circle"></use>
+              <use href="${n}#icon-circle"></use>
             </svg>
-          </li>`}}})});const n=document.querySelectorAll(".gallery");n.forEach(t=>{t.addEventListener("click",e=>{const i=e.target.getAttribute("srcset")||e.target.getAttribute("src");basicLightbox.create(`<img src="${i}" width="800" height="600">`).show()})});
+          </li>`}}})});const r=document.querySelectorAll(".gallery");r.forEach(t=>{t.addEventListener("click",e=>{const i=e.target.getAttribute("srcset")||e.target.getAttribute("src");basicLightbox.create(`<img src="${i}" width="800" height="600">`).show()})});
 //# sourceMappingURL=commonHelpers.js.map

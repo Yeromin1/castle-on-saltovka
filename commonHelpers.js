ae995cf97d695f88bb55ef649a7fd17e10c6759e
Yeromin1 <eremin.dmitriy@ukr.net>
2025-07-22 17:58:49 +0000

    Deploying to gh-pages from @ Yeromin1/castle-on-saltovka@9d603bcf2d33164f667e4c36668e18bb1e41356f 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,7 +1,2 @@
-import"./assets/menu-f265ad14.js";import s from"https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";const n=new URL("/castle-on-saltovka/assets/icons-e70d3d57.svg",self.location);document.addEventListener("DOMContentLoaded",()=>{new s(".premium-swiper",{loop:!0,slidesPerView:1,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",clickable:!0,renderBullet:function(t,e){return`
-          <li class="${e}">
-            <svg class="ball-icon" width="4" height="4">
-              <use href="${n}#icon-circle"></use>
-            </svg>
-          </li>`}}})});const r=document.querySelectorAll(".gallery");r.forEach(t=>{t.addEventListener("click",e=>{const i=e.target.getAttribute("srcset")||e.target.getAttribute("src");basicLightbox.create(`<img src="${i}" width="800" height="600">`).show()})});
+import"./assets/menu-f265ad14.js";import i from"https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";document.addEventListener("DOMContentLoaded",()=>{new i(".premium-swiper",{loop:!0,slidesPerView:1,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",clickable:!0}})});const n=document.querySelectorAll(".gallery");n.forEach(t=>{t.addEventListener("click",e=>{const r=e.target.getAttribute("srcset")||e.target.getAttribute("src");basicLightbox.create(`<img src="${r}" width="800" height="600">`).show()})});
 //# sourceMappingURL=commonHelpers.js.map

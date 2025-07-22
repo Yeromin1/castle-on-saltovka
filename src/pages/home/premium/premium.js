8c31fb68f51f3e8c395cd0712d9e52d6dba108c7
Yeromin Dmytro <eremin.dmitriy@ukr.net>
2025-07-22 19:14:33 +0300

    svgImg

--- src/pages/home/premium/premium.js
+++ src/pages/home/premium/premium.js
@@ -1,5 +1,7 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
 
+const svgImg = new URL('/img/icons.svg', import.meta.url);
+
 document.addEventListener('DOMContentLoaded', () => {
   const swiper = new Swiper('.premium-swiper', {
     loop: true,
@@ -15,7 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
         return `
           <li class="${className}">
             <svg class="ball-icon" width="4" height="4">
-              <use href="./img/icons.svg#icon-circle"></use>
+              <use href="${svgImg}#icon-circle"></use>
             </svg>
           </li>`;
       },

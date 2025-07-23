af09b6f303ec3ca57bc09541cdc175c8d89c6132
Yeromin Dmytro <eremin.dmitriy@ukr.net>
2025-07-23 22:36:27 +0300

    categories@media

--- src/pages/home/categories/categories.js
+++ src/pages/home/categories/categories.js
@@ -32,19 +32,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // Заменяем href на "#"
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('.link-item').forEach(link => {
-    if (
-      link.getAttribute('href') ===
-      '/castle-on-saltovka/pages/catalog/overhead-electronic/overhead-electronic.html'
-    ) {
-      link.setAttribute('href', '#');
-    }
-  });
-
-  // Отменяем переход по всем ссылкам href="#"
-  document.querySelectorAll('a[href="#"]').forEach(link => {
-    link.addEventListener('click', function (event) {
-      event.preventDefault();
+  if (window.matchMedia('(min-width: 768px)').matches) {
+    document.querySelectorAll('.link-item').forEach(link => {
+      if (
+        link.getAttribute('href') ===
+        '/castle-on-saltovka/pages/catalog/overhead-electronic/overhead-electronic.html'
+      ) {
+        link.setAttribute('href', '#');
+      }
     });
-  });
+
+    // Отменяем переход по всем ссылкам href="#"
+    document.querySelectorAll('a[href="#"]').forEach(link => {
+      link.addEventListener('click', function (event) {
+        event.preventDefault();
+      });
+    });
+  }
 });

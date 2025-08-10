7b80077e06bdec45707c765c2f105ed62d0d6e44
Yeromin1 <eremin.dmitriy@ukr.net>
2025-08-10 21:20:08 +0000

    Deploying to gh-pages from @ Yeromin1/castle-on-saltovka@9b2c5d0ee5beac36ae85de7525d334d11dd7ec6b 🚀

--- commonHelpers14.js
+++ commonHelpers14.js
@@ -1,16 +1,29 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";document.addEventListener("DOMContentLoaded",()=>{const r=document.getElementById("order-cart-list"),i=document.getElementById("order-cart-total"),d=localStorage.getItem("cart"),c=d?JSON.parse(d):[];function l(){localStorage.setItem("cart",JSON.stringify(c))}function o(){r.innerHTML="";let a=0;c.forEach(t=>{const e=document.createElement("li");e.className="order-cart-item",e.innerHTML=`
-          <img src="${t.image}" alt="${t.name}" class="order-cart-img" />
+import"./assets/modulepreload-polyfill-3cfb730f.js";document.addEventListener("DOMContentLoaded",()=>{const d=document.getElementById("order-cart-list"),u=document.getElementById("order-cart-total"),i=document.getElementById("edit-cart-btn"),r=new URL("/castle-on-saltovka/assets/icons-e1d2494d.svg",self.location),l=localStorage.getItem("cart"),a=l?JSON.parse(l):[];let o=!1;i.addEventListener("click",()=>{o=!o;const e=i.querySelector(".edit-cart-text"),t=i.querySelector("svg");e&&t&&(o?(e.style.display="none",t.style.display="none"):(e.style.display="",t.style.display="")),n()});function p(){localStorage.setItem("cart",JSON.stringify(a))}function n(){d.innerHTML="";let e=0;a.forEach(t=>{const s=document.createElement("li");s.className="order-cart-item",o&&s.classList.add("edit-mode"),s.innerHTML=`
+        <img src="${t.image}" alt="${t.name}" class="order-cart-img" />
           <div class="order-cart-info">
             <div class="container-cart-name-price">
               <p class="order-cart-name">${t.name}</p>
               <div class="order-cart-controls">
                 <p class="order-cart-price">${t.price.toLocaleString()} грн.</p>
-                <button class="qty-btn minus" data-id="${t.id}" ${t.quantity===1?"disabled":""}>−</button>
+                <button class="qty-btn minus" data-id="${t.id}" ${t.quantity===1?"disabled":""}>
+                  <svg class="qty-minus" width="16" height="16">
+                    <use href="${r}#icon-minus"></use>
+                  </svg>
+                </button>
                 <span class="qty-count">${t.quantity}</span>
-                <button class="qty-btn plus" data-id="${t.id}">+</button>
-              </div>
+                <button class="qty-btn plus" data-id="${t.id}">
+                  <svg class="qty-plus" width="16" height="16">
+                    <use href="${r}#icon-plus"></use>
+                  </svg>
+                </button>
             </div>
-            <button class="remove-item" data-id="${t.id}">Видалити</button>
           </div>
-        `,r.appendChild(e),a+=t.price*t.quantity}),i.innerHTML=`<div class="container-order-cart-total"><span class="order-cart-total">Загальна сума:</span> <span class="order-cart-total-price">${a.toLocaleString()} грн.</span></div>`,l(),u()}function u(){document.querySelectorAll(".qty-btn.plus").forEach(a=>{a.onclick=()=>{const t=a.dataset.id,e=c.find(n=>n.id===t);e&&e.quantity++,o()}}),document.querySelectorAll(".qty-btn.minus").forEach(a=>{a.onclick=()=>{const t=a.dataset.id,e=c.find(n=>n.id===t);e&&e.quantity>1&&e.quantity--,o()}}),document.querySelectorAll(".remove-item").forEach(a=>{a.onclick=()=>{const t=a.dataset.id,e=c.findIndex(n=>n.id===t);e!==-1&&(c.splice(e,1),o())}})}o(),document.getElementById("edit-cart-btn").addEventListener("click",()=>{var n;const a=document.querySelectorAll(".order-cart-item"),t=(n=a[0])==null?void 0:n.classList.contains("edit-mode");a.forEach(s=>{t?s.classList.remove("edit-mode"):s.classList.add("edit-mode")});const e=document.getElementById("edit-cart-btn");e.textContent=t?"Редагувати":""})});
+          <button class="remove-item" data-id="${t.id}">Видалити</button>
+        </div>
+      `,d.appendChild(s),e+=t.price*t.quantity}),u.innerHTML=`
+      <div class="container-order-cart-total">
+        <span class="order-cart-total">Загальна сума:</span>
+        <span class="order-cart-total-price">${e.toLocaleString()} грн.</span>
+      </div>
+    `,p(),y()}function y(){document.querySelectorAll(".qty-btn.plus").forEach(e=>{e.onclick=()=>{const t=e.dataset.id,s=a.find(c=>c.id===t);s&&s.quantity++,n()}}),document.querySelectorAll(".qty-btn.minus").forEach(e=>{e.onclick=()=>{const t=e.dataset.id,s=a.find(c=>c.id===t);s&&s.quantity>1&&s.quantity--,n()}}),document.querySelectorAll(".remove-item").forEach(e=>{e.onclick=()=>{const t=e.dataset.id,s=a.findIndex(c=>c.id===t);s!==-1&&(a.splice(s,1),n())}})}n()});
 //# sourceMappingURL=commonHelpers14.js.map

5e380d41dcc5f5d978107fa18f4c9198538fb1fe
Yeromin Dmytro <eremin.dmitriy@ukr.net>
2026-01-05 23:35:21 +0200

    popularSwiper

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,20 +1,3 @@
-// import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   const swiper = new Swiper('.popular-swiper', {
-//     loop: true,
-//     slidesPerView: 1,
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-//     pagination: {
-//       el: '.swiper-pagination',
-//       clickable: true,
-//     },
-//   });
-// });
-
 document.addEventListener('DOMContentLoaded', () => {
   // Premium swiper
   const premiumSwiper = new Swiper('.premium-swiper', {
@@ -31,12 +14,26 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   // Popular swiper
+  // const popularSwiper = new Swiper('.popular-swiper', {
+  //   loop: false,
+  //   slidesPerView: 3,
+  //   navigation: {
+  //     nextEl: '.popular-swiper .swiper-button-next',
+  //     prevEl: '.popular-swiper .swiper-button-prev',
+  //   },
+  // });
+
   const popularSwiper = new Swiper('.popular-swiper', {
     loop: false,
-    slidesPerView: 3,
     navigation: {
       nextEl: '.popular-swiper .swiper-button-next',
       prevEl: '.popular-swiper .swiper-button-prev',
+    },
+    slidesPerView: 'auto',
+    breakpoints: {
+      1158: {
+        slidesPerView: 3,
+      },
     },
   });
 });
